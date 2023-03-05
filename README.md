4622466e6329e888a45103183414afc6858a4f11
Roberto Inconis <robertoinconis1607@gmail.com>
2023-03-05 14:50:37 +0100

    no debug line for tweet_job

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Scheduled tweets web application. Based from the following tutorial: https://www.youtube.com/playlist?list=PLm8ctt9NhMNV75T9WYIrA6m9I_uw7vS56
+
+Ruby 3.1.2
+Rails 7.0.4
--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -2,7 +2,6 @@ class TweetJob < ApplicationJob
   queue_as :default
 
   def perform(tweet)
-    binding.irb
     return if tweet.published?
     return if tweet.publish_at > Time.current #if a tweet has been rescheduled to the future
     tweet.publish_to_twitter!
